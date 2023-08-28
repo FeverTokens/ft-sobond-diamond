3ce30437c553af61bcf2f8d371d5292d0856e433
mtoub <med.toub@gmail.com>
2023-08-28 13:11:56 +0100

    feat: added utils, data, and rbac facet

---
+++ contracts/access/rbac/IAccessControl.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+// FeverTokens Contracts v1.0.0
+
+pragma solidity ^0.8.20;
+
+interface IAccessControl {
+    /**
+     * @dev Emitted when `newAdminRole` is set as ``role``'s admin role, replacing `previousAdminRole`
+     *
+     * `DEFAULT_ADMIN_ROLE` is the starting admin for all roles, despite
+     * {RoleAdminChanged} not being emitted signaling this.
+     *
+     * _Available since v3.1._
+     */
+    event RoleAdminChanged(
+        bytes32 indexed role,
+        bytes32 indexed previousAdminRole,
+        bytes32 indexed newAdminRole
+    );
+
+    /**
+     * @dev Emitted when `account` is granted `role`.
+     *
+     * `sender` is the account that originated the contract call, an admin role
+     * bearer except when using {AccessControl-_setupRole}.
+     */
+    event RoleGranted(
+        bytes32 indexed role,
+        address indexed account,
+        address indexed sender
+    );
+
+    /**
+     * @dev Emitted when `account` is revoked `role`.
+     *
+     * `sender` is the account that originated the contract call:
+     *   - if using `revokeRole`, it is the admin role bearer
+     *   - if using `renounceRole`, it is the role bearer (i.e. `account`)
+     */
+    event RoleRevoked(
+        bytes32 indexed role,
+        address indexed account,
+        address indexed sender
+    );
+
+    /*
+     * @notice query whether role is assigned to account
+     * @param role role to query
+     * @param account account to query
+     * @return whether role is assigned to account
+     */
+    function hasRole(
+        bytes32 role,
+        address account
+    ) external view returns (bool);
+
+    /*
+     * @notice query admin role for given role
+     * @param role role to query
+     * @return admin role
+     */
+    function getRoleAdmin(bytes32 role) external view returns (bytes32);
+
+    /*
+     * @notice assign role to given account
+     * @param role role to assign
+     * @param account recipient of role assignment
+     */
+    function grantRole(bytes32 role, address account) external;
+
+    /*
+     * @notice unassign role from given account
+     * @param role role to unassign
+     * @parm account
+     */
+    function revokeRole(bytes32 role, address account) external;
+
+    /**
+     * @notice relinquish role
+     * @param role role to relinquish
+     */
+    function renounceRole(bytes32 role) external;
+
+    /**
+     * @notice Returns one of the accounts that have `role`. `index` must be a
+     * value between 0 and {getRoleMemberCount}, non-inclusive.
+     *
+     * Role bearers are not sorted in any particular way, and their ordering may
+     * change at any point.
+     *
+     * WARNING: When using {getRoleMember} and {getRoleMemberCount}, make sure
+     * you perform all queries on the same block. See the following
+     * https://forum.openzeppelin.com/t/iterating-over-elements-on-enumerableset-in-openzeppelin-contracts/2296[forum post]
+     * for more information.
+     */
+    function getRoleMember(
+        bytes32 role,
+        uint256 index
+    ) external view returns (address);
+
+    /**
+     * @notice Returns the number of accounts that have `role`. Can be used
+     * together with {getRoleMember} to enumerate all bearers of a role.
+     */
+    function getRoleMemberCount(bytes32 role) external view returns (uint256);
+}
