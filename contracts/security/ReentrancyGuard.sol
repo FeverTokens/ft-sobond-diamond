60fa56ffc70938d5998b481ad688f6fe36cf7e87
mtoub <med.toub@gmail.com>
2023-08-30 21:09:08 +0100

    fix: added added interfaces (general and internal) and internal functions for ReentrancyGuard

--- contracts/security/ReentrancyGuard.sol
+++ contracts/security/ReentrancyGuard.sol
@@ -3,10 +3,11 @@
 
 pragma solidity ^0.8.20;
 
-import { ReentrancyGuardStorage } from "./ReentrancyGuardStorage.sol";
+import { IReentrancyGuard } from "./IReentrancyGuard.sol";
+import { ReentrancyGuardInternal } from "./ReentrancyGuardInternal.sol";
 
 /**
- * @dev Contract module that helps prevent reentrant calls to a function.
+ * @title Contract module that helps prevent reentrant calls to a function.
  *
  * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
  * available, which can be applied to functions to make sure there are no nested
@@ -21,49 +22,6 @@ import { ReentrancyGuardStorage } from "./ReentrancyGuardStorage.sol";
  * to protect against it, check out our blog post
  * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
  */
-abstract contract ReentrancyGuard {
-    // Booleans are more expensive than uint256 or any type that takes up a full
-    // word because each write operation emits an extra SLOAD to first read the
-    // slot's contents, replace the bits taken up by the boolean, and then write
-    // back. This is the compiler's defense against contract upgrades and
-    // pointer aliasing, and it cannot be disabled.
+abstract contract ReentrancyGuard is IReentrancyGuard, ReentrancyGuardInternal {
 
-    // The values being non-zero value makes deployment a bit more expensive,
-    // but in exchange the refund on every call to nonReentrant will be lower in
-    // amount. Since refunds are capped to a percentage of the total
-    // transaction's gas, it is best to keep them low in cases like this one, to
-    // increase the likelihood of the full refund coming into effect.
-    uint256 private constant _ENTERED = 2;
-    uint256 private constant _NOT_ENTERED = 1;
-
-    modifier nonReentrant() virtual {
-        // On the first call to nonReentrant, status will be 0
-        require(
-            ReentrancyGuardStorage.layout().status != _ENTERED,
-            "ReentrancyGuard: reentrant call"
-        );
-
-        // Any calls to nonReentrant after this point will fail
-        _lockReentrancyGuard();
-
-        _;
-
-        // By storing the original value once again, a refund is triggered (see
-        // https://eips.ethereum.org/EIPS/eip-2200)
-        _unlockReentrancyGuard();
-    }
-
-    /**
-     * @notice lock functions that use the nonReentrant modifier
-     */
-    function _lockReentrancyGuard() internal virtual {
-        ReentrancyGuardStorage.layout().status = _ENTERED;
-    }
-
-    /**
-     * @notice unlock functions that use the nonReentrant modifier
-     */
-    function _unlockReentrancyGuard() internal virtual {
-        ReentrancyGuardStorage.layout().status = _NOT_ENTERED;
-    }
 }
