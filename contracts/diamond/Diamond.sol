c44d80270c1e025024119465353657e3e7f51423
mtoub <med.toub@gmail.com>
2023-09-01 02:34:30 +0100

    feat: added Diamond proxy

---
+++ contracts/diamond/Diamond.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// FeverTokens Contracts v1.0.0
+
+pragma solidity ^0.8.20;
+
+import { ERC165, ERC165Internal } from "../introspection/ERC165.sol";
+
+contract Diamond is ERC165Internal, ERC165 {
+    // TODO add Proxy for fallback function
+}
